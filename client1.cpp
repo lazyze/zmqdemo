79b309462d2dd1cec89ff94a3c744e5dd31ed034
lize <GW00301491@ifyou.com>
2022-12-23 16:32:37 +0800

    多客户端同时在线，支持停止，暂停，关闭客户端连接操作

--- client1.cpp
+++ client1.cpp
@@ -2,7 +2,7 @@
  * @Author: lize GW00301491@ifyou.com
  * @Date: 2022-12-23 12:29:47
  * @LastEditors: lize GW00301491@ifyou.com
- * @LastEditTime: 2022-12-23 13:22:54
+ * @LastEditTime: 2022-12-23 16:30:40
  * @FilePath: /test/home/lize/code/zmqdemo/client1.cpp
  * @Description: 多客户端
  * 
@@ -29,30 +29,38 @@ int main (int argc, char **argv)
     // ZMQ_REQ  客户端使用ZMQ_REQ类型的套接字向服务发送请求并从服务接收答复
     void *requester = zmq_socket (context, ZMQ_REQ);
     zmq_connect (requester, "tcp://localhost:8000");
+    char push_buf[64] = "";
+    char sub_buf[64] = "";
 
     //创建pull套接字
     void *pull_socket = zmq_socket (context, ZMQ_PULL);
-    
 
-    // int port = std::stoi(std::string(argv[1]));
-
-    // char pull_ip[30] = "tcp://192.168.61.5:";
-    
-    // while (port != 0) {
-    //     pull_ip[strlen(pull_ip)] = '0' + port % 10;
-    //     port /= 10;
-    // }
     string ip = "tcp://192.168.61.5:" + std::string(argv[1]);
     const char * pull_ip = ip.c_str();
     int ret = zmq_bind (pull_socket, pull_ip);
     assert(ret == 0);
 
     printf ("发送pull的ip和端口 %s...\n", pull_ip);
-
-
-
     zmq_send (requester, pull_ip, strlen(pull_ip), 0);
 
+    zmq_recv(requester, push_buf, sizeof(push_buf), 0);
+    printf ("接收到服务器push回复消息: %s...\n", push_buf);
+
+    //创建pub套接字
+    void *pub_socket = zmq_socket (context, ZMQ_PUB);
+    ip = "tcp://192.168.61.5:" + std::string(argv[2]);
+    const char * pub_ip = ip.c_str();
+    ret = zmq_bind (pub_socket, pub_ip);
+    assert(ret == 0);
+
+    printf ("发送pub的ip和端口 %s...\n", pub_ip);
+    zmq_send (requester, pub_ip, strlen(pub_ip), 0);
+
+    zmq_recv(requester, sub_buf, sizeof(sub_buf), 0);
+    printf ("接收到服务器sub回复消息: %s...\n", sub_buf);
+
+    sleep(5);
+    zmq_send (pub_socket, "cmd:begin", 9, 0);
 
     ///3.循环pull数据
     int num = 0;
@@ -61,9 +69,23 @@ int main (int argc, char **argv)
         char buffer[64] = "";
         zmq_recv (pull_socket, buffer, 63, 0);
         printf ("接收到消息 %s\n", buffer);
+        num++;
+        if(num == 20) {
+            zmq_send (pub_socket, "cmd:stop", 8, 0);
+            sleep(25);
+            zmq_send (pub_socket, "cmd:begin", 9, 0);
+        }
+
+
+        if(num == 60) {
+            zmq_send (pub_socket, "cmd:close", 9, 0);
+            break;
+        }
     }
     /// 6.关闭套接字、销毁上下文
     zmq_close (requester);
+    zmq_close (pull_socket);
+    zmq_close(pub_socket);
     zmq_ctx_destroy (context);
     #pragma endregion
     #endif
--- server1.cpp
+++ server1.cpp
@@ -2,7 +2,7 @@
  * @Author: lize GW00301491@ifyou.com
  * @Date: 2022-12-21 17:52:21
  * @LastEditors: lize GW00301491@ifyou.com
- * @LastEditTime: 2022-12-23 13:24:37
+ * @LastEditTime: 2022-12-23 15:48:00
  * @FilePath: /test/home/lize/code/zmqdemo/server1.cpp
  * @Description: 支持多客户端同时在线
  *
@@ -20,10 +20,11 @@ using namespace std;
 
 #define CLIENT_NUM 3
 void* push_socket[CLIENT_NUM];
-void *sub_socket[CLIENT_NUM];
+void* sub_socket[CLIENT_NUM];
+bool push_flag[CLIENT_NUM];
 int sendNum = 0;
 
-void itemAssign(zmq_pollitem_t& item, void* socket) {
+void itemAdd(zmq_pollitem_t& item, void* socket) {
     item.socket = socket;
     item.fd = 0;
     item.events = ZMQ_POLLIN;
@@ -41,12 +42,12 @@ void sendMessage(int n) {
     buf += to_string(n);
     int num = 0;
     for (int i = 0; i < CLIENT_NUM; i++) {
-        if(push_socket[i] != NULL) {
+        if(push_socket[i] != NULL && push_flag[i]) {
             num++;
             zmq_send(push_socket[i], buf.c_str(), strlen(buf.c_str()), 0);
         }
     }
-    printf("第%d次发送消息, 发送到%d个客户端", sendNum, num);
+    printf("第%d次发送消息, 发送到%d个客户端\n", sendNum, num);
 }
 
 // void sockerAssign(void *socket[]) {
@@ -77,13 +78,14 @@ int main (void)
     //zmq_poll参数赋值
     zmq_pollitem_t *items =(zmq_pollitem_t *)malloc((CLIENT_NUM + 1) * sizeof(zmq_pollitem_t));
     for (int i = 0; i < CLIENT_NUM; i++) {
-        itemAssign(items[i], NULL);
+        itemAdd(items[i], NULL);
     }
-    itemAssign(items[CLIENT_NUM], responder);
+    itemAdd(items[CLIENT_NUM], responder);
     //push_socket套接字赋值
     // sockerAssign(push_socket);
     for (int i = 0; i < CLIENT_NUM; i++) {
         push_socket[i] = NULL;
+        push_flag[i] = false;
     }
 
     int num = 1;
@@ -102,19 +104,67 @@ int main (void)
             for (int i = 0; i < CLIENT_NUM; i++) {
                  if(push_socket[i] == NULL) {
                     push_socket[i] = zmq_socket(context, ZMQ_PUSH);
+                    sub_socket[i] = zmq_socket(context, ZMQ_SUB);
 
                     //接收客户端发过来的pull套接字端口信息，发送消息到此套接字接收
                     char client_pull_addr[26] = "";
                     printf("waiting client %d connect......\n", i);
-                    int rc = zmq_recv(responder, client_pull_addr, 25, 0);
-                    printf("接收返回值rc = %d, 接收到客户端%d发送的pull的ip和端口 = %s\n", rc, i, client_pull_addr);
+                    ret = zmq_recv(responder, client_pull_addr, 25, 0);
+                    printf("接收返回值rc = %d, 接收到客户端%d发送的pull的ip和端口 = %s\n", ret, i, client_pull_addr);
                     zmq_send(responder, "received pull", 13, 0);
 
                     //push与pull建立连接
                     ret = zmq_connect(push_socket[i], client_pull_addr);
                     assert (ret == 0);
+                    // push_flag[i] = true;
+
+                    //接收客户端发过来的pub套接字端口信息,接收此套接字发过来的开始,停止,结束等信息
+                    char client_sub_addr[26] = "";
+                    ret = zmq_recv(responder, client_sub_addr, 25, 0);
+                    printf("接收返回值rc = %d, 接收到客户端%d发送的pub的ip和端口 = %s\n", ret, i, client_sub_addr);
+                    zmq_send(responder, "received pub", 12, 0);
+
+                    //pub与sub建立连接,且设置订阅过滤信息
+                    ret = zmq_connect(sub_socket[i], client_sub_addr);
+                    assert (ret == 0);
+                    ret = zmq_setsockopt(sub_socket[i], ZMQ_SUBSCRIBE, "cmd:", 4);
+                    assert (ret == 0);
+
+                    //将pub加入items
+                    itemAdd(items[i], sub_socket[i]);
                     break;
                  }
+            }
+        }
+        //检测到pub事件
+        for (int i = 0; i < CLIENT_NUM; i++) {
+            if (items[i].revents & ZMQ_POLLIN) {
+                zmq_msg_t msg;
+                zmq_msg_init(&msg);
+                char *cmd_info;
+                int size = zmq_msg_recv(&msg, items[i].socket, 0);
+                cmd_info = (char*)zmq_msg_data(&msg);
+                cmd_info[zmq_msg_size(&msg)] = 0;
+                printf("接收client操作指令为%s\n", cmd_info);
+                char * cmd_op_info = cmd_info + 4;
+                if (strcmp(cmd_op_info, "begin") == 0) {  //开始接收
+                    printf("接收到开始接收信息\n");
+                    push_flag[i] = true;
+                } else if (strcmp(cmd_op_info, "stop") == 0) {   //暂停接收
+                    printf("接收到暂停接收信息\n");
+                    push_flag[i] = false;
+                } else if (strcmp(cmd_op_info, "close") == 0) {  //模仿关闭client
+                    zmq_close(push_socket[i]);
+                    zmq_close(sub_socket[i]);
+                    push_socket[i] = NULL;
+                    push_flag[i] = false;
+                    sub_socket[i] = NULL;
+                    items[i].socket = NULL;
+                    printf("客户端%d关闭\n", i);
+                    sleep(5);
+                } else {
+                    printf("无效指令\n");
+                }
             }
         }
         sleep(3);
