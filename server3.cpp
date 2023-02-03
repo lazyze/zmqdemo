bab999b91afb12b125d558d3328099e8c87b98ef
lize <GW00301491@ifyou.com>
2023-02-03 15:36:38 +0800

    代理模式，代理仅做转发操作

--- server3.cpp
+++ server3.cpp
@@ -2,7 +2,7 @@
  * @Author: lize GW00301491@ifyou.com
  * @Date: 2022-12-21 17:52:21
  * @LastEditors: lize GW00301491@ifyou.com
- * @LastEditTime: 2023-02-03 15:32:19
+ * @LastEditTime: 2023-02-03 15:35:44
  * @FilePath: /test/home/lize/code/zmqdemo/server1.cpp
  * @Description: 代理模式
  *
@@ -89,7 +89,7 @@ int main(void) {
 
   // zmq_poll参数赋值
   zmq_pollitem_t *items =
-      (zmq_pollitem_t *)malloc((CLIENT_NUM + 2) * sizeof(zmq_pollitem_t));
+      (zmq_pollitem_t *)malloc((CLIENT_NUM + 1) * sizeof(zmq_pollitem_t));
   for (int i = 0; i < CLIENT_NUM; i++) {
     itemAdd(items[i], NULL);
   }
@@ -101,7 +101,7 @@ int main(void) {
   }
 
   int ret = zmq_bind(proxyResponder, "tcp://*:8000");
-  itemAdd(items[CLIENT_NUM + 1], proxyResponder);
+  // itemAdd(items[CLIENT_NUM + 1], proxyResponder);
   if (ret == 0) {
     printf("申请代理成功\n");
     // proxyFlag = true;
@@ -116,15 +116,14 @@ int main(void) {
     sleep(1);
 
     while (1) {
-      ret = zmq_poll(items, CLIENT_NUM + 2, -1);
-      if (ret < 0) {
-        printf("zmq_poll返回值为%d\n", ret);
-        break;
-      }
+      // ret = zmq_poll(items, CLIENT_NUM + 2, -1);
+      // if (ret < 0) {
+      //   printf("zmq_poll返回值为%d\n", ret);
+      //   break;
+      // }
       // printf("zmq_poll返回值为%d\n", ret);
 
       // 检测到客户端发送来的连接
-      if (items[CLIENT_NUM + 1].revents & ZMQ_POLLIN) {
         printf("client connecting......\n");
         char client_pull_addr[26] = "";
         ret = zmq_recv(proxyResponder, client_pull_addr, 25, 0);
@@ -132,100 +131,6 @@ int main(void) {
                client_pull_addr);
         zmq_send(proxyResponder, client_pull_addr, strlen(client_pull_addr), 0);
         zmq_send(proxyPublisher, client_pull_addr, strlen(client_pull_addr), 0);
-      }
-      // 检测到代理发送的信息
-      if (items[CLIENT_NUM].revents & ZMQ_POLLIN) {
-        for (int i = 0; i < CLIENT_NUM; i++) {
-          if (push_socket[i] == NULL) {
-            push_socket[i] = zmq_socket(context, ZMQ_PUSH);
-            sub_socket[i] = zmq_socket(context, ZMQ_SUB);
-
-            // 接收客户端发过来的pull套接字端口信息，发送消息到此套接字接收
-            char client_pull_addr[26] = "";
-            printf("waiting client %d connect......\n", i);
-            ret = zmq_recv(responder, client_pull_addr, 25, 0);
-            printf(
-                "接收返回值rc = %d, 接收到客户端%d发送的pull的ip和端口 = %s\n",
-                ret, i, client_pull_addr);
-            // zmq_send(responder, "received pull", 13, 0);
-
-            // push与pull建立连接
-            ret = zmq_connect(push_socket[i], client_pull_addr);
-            assert(ret == 0);
-            // push_flag[i] = true;
-
-            // 接收客户端发过来的pub套接字端口信息,接收此套接字发过来的开始,停止,结束等信息
-            char client_sub_addr[26] = "";
-            ret = zmq_recv(responder, client_sub_addr, 25, 0);
-            printf(
-                "接收返回值rc = %d, 接收到客户端%d发送的pub的ip和端口 = %s\n",
-                ret, i, client_sub_addr);
-            // zmq_send(responder, "received pub", 12, 0);
-
-            // pub与sub建立连接,且设置订阅过滤信息
-            ret = zmq_connect(sub_socket[i], client_sub_addr);
-            assert(ret == 0);
-            ret = zmq_setsockopt(sub_socket[i], ZMQ_SUBSCRIBE, "cmd:", 4);
-            assert(ret == 0);
-
-            // 将pub加入items
-            itemAdd(items[i], sub_socket[i]);
-            break;
-          }
-        }
-      }
-      // 检测到pub事件
-      for (int i = 0; i < CLIENT_NUM; i++) {
-        if (items[i].revents & ZMQ_POLLIN) {
-          zmq_msg_t msg;
-          zmq_msg_init(&msg);
-          char *cmd_info;
-          int size = zmq_msg_recv(&msg, items[i].socket, 0);
-          cmd_info = (char *)zmq_msg_data(&msg);
-          cmd_info[zmq_msg_size(&msg)] = 0;
-          printf("接收client操作指令为%s\n", cmd_info);
-          char *cmd_op_info = cmd_info + 4;
-          if (strcmp(cmd_op_info, "begin") == 0) {  // 开始接收
-            printf("接收到开始接收信息\n");
-            push_flag[i] = true;
-            liveness[i] = HEARTBEAT_LIVENESS;
-          } else if (strcmp(cmd_op_info, "stop") == 0) {  // 暂停接收
-            printf("接收到暂停接收信息\n");
-            push_flag[i] = false;
-          } else if (strcmp(cmd_op_info, "close") == 0) {  // 模仿关闭client
-            zmq_close(push_socket[i]);
-            zmq_close(sub_socket[i]);
-            push_socket[i] = NULL;
-            push_flag[i] = false;
-            sub_socket[i] = NULL;
-            items[i].socket = NULL;
-            printf("接收到close命令,客户端%d关闭\n", i);
-            sleep(5);
-          } else if (strcmp(cmd_op_info, "heart") == 0) {  // 模仿关闭client
-            liveness[i] = HEARTBEAT_LIVENESS;
-          } else {
-            printf("无效指令\n");
-          }
-        } else if (--liveness[i] == 0) {
-          // 检测活跃度
-          zmq_close(push_socket[i]);
-          zmq_close(sub_socket[i]);
-          push_socket[i] = NULL;
-          push_flag[i] = false;
-          sub_socket[i] = NULL;
-          items[i].socket = NULL;
-          printf("模拟异常退出关闭进程, 客户端%d关闭\n", i);
-          // sleep(5);
-        }
-        // printf();
-      }
-      if (!proxyFlag) {
-        responder = zmq_socket(context, ZMQ_SUB);
-        zmq_connect(responder, "tcp://10.66.150.1:8002");
-        zmq_setsockopt(responder, ZMQ_SUBSCRIBE, "tcp://:", 6);
-        itemAdd(items[CLIENT_NUM], responder);
-        proxyFlag = true;
-      }
     }
   } else {
     printf("申请代理失败\n");
@@ -239,7 +144,7 @@ int main(void) {
   // for (int i = 0; i < CLIENT_NUM; i++) {
   //   itemAdd(items[i], NULL);
   // }
-  // itemAdd(items[CLIENT_NUM], responder);
+  itemAdd(items[CLIENT_NUM], responder);
   // // push_socket套接字赋值
   // //  sockerAssign(push_socket);
   // for (int i = 0; i < CLIENT_NUM; i++) {
